c3955a7cb77ce8abe515da929945e6fd18f49ec4
botedit <137023430+botedit@users.noreply.github.com>
2023-06-19 17:16:08 +0300

    Update botedit.js

--- botedit.js
+++ botedit.js
@@ -3,7 +3,7 @@ const { Telegraf } = require("telegraf");
 const bot = new Telegraf("5560270744:AAFc-zD5muAuE5ILasQwAZ68cf6m7Mw2QUE");
 
 
-bot.on('edited_message', ctx => {
+bot.on('edited_messageMedia', ctx => {
   let msg_id = ctx.editMessageMedia.message_id
   ctx.telegram.deleteMessage(ctx.chat.id, msg_id)
   ctx.reply( "@"+ctx.from.username +   ` لا تعدل يا ورع ممنوع `)
